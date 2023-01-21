9435b1f523ed4f9f039660d3fa7bfff35ba0f765
berhanez <birhanez@gmail.com>
2023-01-21 11:02:50 +0300

    implementation of insertion sort algorith

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ * swap_nodes - Swap two nodes in a doubly-linked list, listint_t
+ * @h: pointer to the head of the doubly-linked list
+ * @n1: pointer to the first node to swap
+ * @n2: 2nd node to swap
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of ints
+ *                       using the insertion sort algorithm
+ * @list: pointer to the head of a doubly-linked list of ints
+ * Description: Prints the list after each swap.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *insert, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (i = (*list)->next; i != NULL; i = temp)
+	{
+		temp = i->next;
+		insert = i->prev;
+		while (insert != NULL && i->n < insert->n)
+		{
+			swap_nodes(list, &insert, i);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
